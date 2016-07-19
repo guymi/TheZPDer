1c0a745c782d03a7357537f4333e52f605867aba
guy <guyt@feedvisor.com>
2016-07-19 11:54:31 +0300

    adding backend

---
+++ backend/src/main/java/org/tikal/fuse/Application.java
@@ -0,0 +1,15 @@
+package org.tikal.fuse;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * Created by guy on 7/19/16.
+ */
+@SpringBootApplication
+public class Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Application.class);
+    }
+}
---
+++ backend/src/main/java/org/tikal/fuse/controller/UserController.java
@@ -0,0 +1,18 @@
+package org.tikal.fuse.controller;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * Created by guy on 7/19/16.
+ */
+@RestController
+@RequestMapping("/user")
+public class UserController {
+
+//    @RequestMapping("/{id}",method = RequestMethod.GET)
+//    public boolean health() {
+//        return true;
+//    }
+}
